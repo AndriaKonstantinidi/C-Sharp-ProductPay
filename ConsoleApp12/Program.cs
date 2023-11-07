6167864e8798df6b71de6e69fac303d2c3206faf
visual studio <vs@itstep.ge>
2023-11-07 19:52:34 +0400

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,22 @@
+﻿//using System.IO;
+
+//string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+@"/Test";
+
+//DirectoryInfo directory = new DirectoryInfo(path);
+
+//if (!directory.Exists)
+//    directory.Create();
+
+//var subDir = directory.CreateSubdirectory(directory.Name+@"/SubDir");
+//Console.WriteLine(subDir.Parent);
+
+//foreach(var item in Directory.GetLogicalDrives())
+//{
+//    Console.WriteLine(item);
+//}
+
+using ConsoleApp12;
+
+product pd = new product();
+
+pd.MonthlyPay();
---
+++ ConsoleApp12/product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class product
+    {
+        public int ProductPrice { get; set; }
+        public int ProductQuantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ProductPrice} {ProductQuantity}";
+        }
+
+        public void MonthlyPay()
+        {
+            double starter = 1000;
+
+            double pay = starter / 5 + 10;
+
+            Console.WriteLine($"Starter Price was {starter}");
+            Console.WriteLine($"Payment was {pay}");
+            Console.WriteLine("Would you like to pay in advance");
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                   Console.WriteLine($"new payment {starter += starter * 0.1}");
+                    break;
+
+              case 2:
+                    Console.WriteLine("Cancelled");
+                    break;
+            }
+        }
+        }
+  }
